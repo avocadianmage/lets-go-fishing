8c2962fc5439f45fcb5ea4f154e0c584e7f27786
avocadianmage <claytongotwals@gmail.com>
2021-01-19 00:07:13 -0600

    slight refactor and throttling fix

--- src/services/cardInfoSvc.tsx
+++ src/services/cardInfoSvc.tsx
@@ -21,8 +21,7 @@ class CardInfoSvc {
     }
 
     getCardImageUrl({ id, name, set }: CardInfo) {
-        return new Promise(resolve => {
-
+        return new Promise(async (resolve) => {
             // Check if the URL is already stored in the local cache.
             const cachedUrl = this.cache[id];
             if (cachedUrl) {
@@ -30,31 +29,30 @@ class CardInfoSvc {
                 return;
             }
 
-            DatabaseService.getCardBlob(name).then(blob => {
+            // Check if the blob is already stored in the IndexedDB.
+            const blobFromIDB = await DatabaseService.getCardBlob(name);
+            if (blobFromIDB) {
+                resolve(this.processBlob(id, blobFromIDB));
+                return;
+            }
 
-                // Check if the blob is already stored in the IndexedDB.
-                if (blob) {
-                    resolve(this.processBlob(id, blob));
-                    return;
-                }
-
-                this.outgoingThrottle = this.outgoingThrottle
-                    // Fetch card information from external site.
-                    .then(() => fetch(getQueryUrl(name, set)))
-                    .then(result => result.json())
-                    // Store the fetched image to the database as a blob.
-                    .then(json => {
-                        getPromisedTimeout()
-                            .then(() => fetch(json.image_uris.normal))
-                            .then(response => response.blob())
-                            .then(blob => {
-                                DatabaseService.putCardBlob(blob, name);
-                                resolve(this.processBlob(id, blob));
-                            });
-                    })
-                    // Ensure the next request is throttled.
-                    .then(getPromisedTimeout);
-            });
+            this.outgoingThrottle = this.outgoingThrottle
+                // Fetch card information from external site.
+                .then(() => fetch(getQueryUrl(name, set)))
+                .then(result => result.json())
+                // Store the fetched image to the database as a blob.
+                .then(json => {
+                    this.outgoingThrottle = this.outgoingThrottle
+                        .then(getPromisedTimeout)
+                        .then(() => fetch(json.image_uris.normal))
+                        .then(response => response.blob())
+                        .then(blob => {
+                            DatabaseService.putCardBlob(blob, name);
+                            resolve(this.processBlob(id, blob));
+                        });
+                })
+                // Ensure the next request is throttled.
+                .then(getPromisedTimeout);
         });
     }
 }
