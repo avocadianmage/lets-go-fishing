f941d4b619341ea3fa6449032615d2d78ab8ea75
avocadianmage <claytongotwals@gmail.com>
2022-10-21 23:34:42 -0500

    additional styling for library search

--- client/src/components/gameLayout/librarySearch.tsx
+++ client/src/components/gameLayout/librarySearch.tsx
@@ -11,8 +11,10 @@ export const LibrarySearch = ({ open, contents }: LibrarySearchProps) => {
         <Backdrop open={open}>
             <Autocomplete
                 autoSelect
+                autoHighlight
+                open={open}
                 options={contents.map(option => option.card.name)}
-                sx={{ width: 400 }}
+                sx={{ width: 400, background: 'var(--nord0)' }}
                 renderInput={(params) => (
                     <TextField
                         {...params}
