6d0061b3c58bc1320dae044881a6aec23a84e54a
avocadianmage <claytongotwals@gmail.com>
2022-10-31 00:14:09 -0500

    tweak zone hover styling

--- client/src/utilities/constants.tsx
+++ client/src/utilities/constants.tsx
@@ -1,7 +1,7 @@
 // Layout
 export const [CARD_WIDTH_PX, CARD_HEIGHT_PX] = [244, 340];
 export const ZONE_PADDING_PX = 6;
-export const ZONE_BORDER_PX = 4;
+export const ZONE_BORDER_PX = 2;
 
 // Gameplay
 export const STARTING_HAND_SIZE = 7;
