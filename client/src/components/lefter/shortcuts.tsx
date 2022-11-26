dbd68717033b6291ef05c5e2e3c11eaf16788aba
avocadianmage <claytongotwals@gmail.com>
2022-11-26 12:08:11 -0600

    refactor shortcuts

--- client/src/components/lefter/shortcuts.tsx
+++ client/src/components/lefter/shortcuts.tsx
@@ -1,8 +1,8 @@
-import { Paper, Typography, Divider, TypographyProps, Box } from '@mui/material';
+import { Box, Divider, Paper, Typography } from '@mui/material';
 
-const SingleShortcut = (props: TypographyProps) => (
-    <Typography variant='overline' lineHeight={1} {...props}>
-        {props.children}
+const SingleShortcut = (props: { shortcutKey: string; description: string }) => (
+    <Typography variant='overline' lineHeight={1}>
+        {props.shortcutKey + ' \u{2014} ' + props.description}
     </Typography>
 );
 
@@ -13,6 +13,7 @@ const shortcutColProps = {
     gap: '6px',
     p: '8px 0',
 };
+
 export const Shortcuts = () => (
     <Paper sx={{ marginBottom: '16px', p: '4px 12px' }}>
         <Typography variant='overline'>Keyboard shortcuts</Typography>
@@ -20,14 +21,14 @@ export const Shortcuts = () => (
 
         <Box sx={{ display: 'flex' }}>
             <Box sx={shortcutColProps}>
-                <SingleShortcut>R – Restart</SingleShortcut>
-                <SingleShortcut>K – Search library</SingleShortcut>
-                <SingleShortcut>S – Shuffle library</SingleShortcut>
+                <SingleShortcut shortcutKey='R' description='restart game' />
+                <SingleShortcut shortcutKey='L' description='search library' />
+                <SingleShortcut shortcutKey='S' description='shuffle library' />
             </Box>
             <Box sx={shortcutColProps}>
-                <SingleShortcut>N – Next turn</SingleShortcut>
-                <SingleShortcut>U – Untap all</SingleShortcut>
-                <SingleShortcut>D – Draw</SingleShortcut>
+                <SingleShortcut shortcutKey='N' description='next turn' />
+                <SingleShortcut shortcutKey='U' description='untap all' />
+                <SingleShortcut shortcutKey='D' description='draw one' />
             </Box>
         </Box>
     </Paper>
