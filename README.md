eca3ddaa40e0176846e6e65388dcc072b93f7a6b
avocadianmage <claytongotwals@gmail.com>
2024-02-10 21:54:00 -0600

    add fallback card looking if invalid set

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Let's Go Fishing
 
-<em>Let's Go Fishing</em> is a playtesting ("goldfishing") tool for <em>Magic: the Gathering</em>, with an initial focus on the Commander format.
+<em>Let's Go Fishing</em> is a playtesting ("goldfishing") tool for <em>Magic: the Gathering</em>.
--- src/services/cardInfoSvc.tsx
+++ src/services/cardInfoSvc.tsx
@@ -8,9 +8,6 @@ let promiseChain: Promise<[front: string, back: string]> = Promise.resolve(['', 
 const getPromisedTimeout = () => new Promise((r) => setTimeout(r, QUERY_THROTTLE_MS));
 
 const getQueryUrl = (name: string, set: string): string => {
-    // Map set values from Moxfield to Scryfall.
-    if (set.toLowerCase() === 'mb1') set = 'PLST';
-
     name = encodeURIComponent(name);
     set = encodeURIComponent(set);
     return `https://api.scryfall.com/cards/named?exact=${name}&set=${set}`;
@@ -34,11 +31,43 @@ const saveToBlob = async (
     return processBlob(blob, name, set, isTransformed);
 };
 
-const getImageUrlFromJson = (json: any, imageUrisNode: any): any => {
-    // Log any unexpected response.
-    if (!imageUrisNode) console.log(json);
-    return imageUrisNode.normal;
-}
+const coreGetCardImageUrl = async (
+    name: string,
+    set: string
+): Promise<[front: string, back: string]> => {
+    const result = await fetch(getQueryUrl(name, set));
+
+    if (result.status !== 200) {
+        if (!set) {
+            // Card lookup failed even without a set specified.
+            console.log('Card lookup failed for: ' + { name, set });
+            return ['', ''];
+        }
+
+        // Query without the set parameter and pick the first result.
+        await getPromisedTimeout();
+        return await coreGetCardImageUrl(name, '');
+    }
+
+    const json = await result.json();
+    const jsonNodeForTwoSidedCard = json.card_faces;
+    const jsonNodeForOneSidedCardImage = json.image_uris;
+
+    let frontUrlRemote = '';
+    let backUrl = '';
+    if (jsonNodeForTwoSidedCard && jsonNodeForTwoSidedCard[0].image_uris) {
+        frontUrlRemote = jsonNodeForTwoSidedCard[0].image_uris.normal;
+        const backUrlRemote = jsonNodeForTwoSidedCard[1].image_uris.normal;
+        // Esnure back image is saved before front to avoid loading interruptions causing
+        // desyncs.
+        backUrl = await saveToBlob(backUrlRemote, name, set, true);
+    } else {
+        frontUrlRemote = jsonNodeForOneSidedCardImage.normal;
+    }
+    const frontUrl = await saveToBlob(frontUrlRemote, name, set, false);
+
+    return [frontUrl, backUrl];
+};
 
 export const GetCardImageUrl = async (
     name: string,
@@ -61,25 +90,6 @@ export const GetCardImageUrl = async (
     }
 
     // Fetch card from external web service.
-    promiseChain = promiseChain.then(getPromisedTimeout).then(async () => {
-        const result = await fetch(getQueryUrl(name, set));
-        const json = await result.json();
-        const cardFaces = json.card_faces;
-
-        let frontUrlRemote = '';
-        let backUrl = '';
-        if (cardFaces && cardFaces[0].image_uris) {
-            frontUrlRemote = getImageUrlFromJson(json, cardFaces[0].image_uris);
-            const backUrlRemote = getImageUrlFromJson(json, cardFaces[1].image_uris);
-            // Esnure back image is saved before front to avoid loading interruptions causing 
-            // desyncs.
-            backUrl = await saveToBlob(backUrlRemote, name, set, true);
-        } else {
-            frontUrlRemote = getImageUrlFromJson(json, json.image_uris);
-        }
-        const frontUrl = await saveToBlob(frontUrlRemote, name, set, false);
-
-        return [frontUrl, backUrl];
-    });
+    promiseChain = promiseChain.then(getPromisedTimeout).then(() => coreGetCardImageUrl(name, set));
     return promiseChain;
 };
